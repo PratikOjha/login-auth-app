10f937e19e73246220f1d9dde4fa2f0e395ef119
Pratik Ojha <ojhapratik111@gmail.com>
2023-03-04 15:31:27 +0530

    customer login auth react app with auth0 integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import Logout from "./components/Logout";
+import User from "./components/User";
+import Login from "./components/Login";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isAuthenticated ? (
+        <div>
+          <p>Customer Auth0 Login App</p>
+          <Login />
+        </div>
+      ) : (
+        <div>
+          <Logout />
+          <User />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      )}
     </div>
   );
 }
